d42a6bccc774a5a93db6d470895cc6d871364591
Datn7 <datunashvili72@gmail.com>
2025-07-02 18:11:20 +0400

    added applicationdbcontext

---
+++ EmandAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmandAPI.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmandAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Policy> Policies { get; set; }
+        public DbSet<Claim> Claims { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+    }
+}
